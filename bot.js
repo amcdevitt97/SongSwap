4bc669dd9df2f5d47869c520f7ef3e1269592188
amcdevitt97 <amcdevitt97@gmail.com>
2019-08-19 00:33:04 -0400

    Spotify links that return apple artist links work! Need to work on apple album links, and apple -> spotify integration

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 var TelegramBot = require('node-telegram-bot-api'); 
 var token = require('./config/config.js').authToken;
 const bot = new TelegramBot(token, {polling: true});
-
+var found = false;
 //---------------   SPOTIFY API - With Wrapper     ------------------
 
 //  https://github.com/thelinmichael/spotify-web-api-node#installation
@@ -35,7 +35,10 @@ const {google} = require('googleapis');
 const customsearch = google.customsearch('v1');
 var customSearchKey = require('./config/config.js').customSearchKey; 
 var customSearchID = require('./config/config.js').customSearchID; 
+var artistRegex = /https?:\/\/(music\.)?apple\.com\/([-a-zA-Z]{2})\/artist\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+var albumRegex = /https?:\/\/(music\.)?apple\.com\/([-a-zA-Z]{2})\/album\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
 
+// Using google custom search, look for songs in apple music.
 async function runSearch(query) {
   const res = await customsearch.cse.list({
     cx: customSearchID ,
@@ -44,6 +47,74 @@ async function runSearch(query) {
   });
   return res.data.items[0].link;
 }
+
+// Based on the link returned, determine if it is an album or artist link
+function searchAppleMusicLink(query, msg, title){
+  runSearch(query).then(function(link){
+    if(link.match(artistRegex)){
+      fromAppleArtistLink(link, msg, title);
+    }
+    else if(link.match(albumRegex)){
+      // TODO : store all list items (li) with ' "targetId": ' in their 
+      // properties list, and put all the ones with numbers following 
+      // it into an array. Pull the spotify track number and get the
+      // song id from the given array based on that track number. 
+      bot.sendMessage(msg.chat.id, "ALBUM LINK: "+ link, {});
+    }
+    
+    // This is excessive scraping but this is what Apple gets
+    // when they make their Music API 99 dollars to use... 
+  });
+}
+
+// Link is an artist link, get all links embedded in the site with song ids.
+function fromAppleArtistLink(link, msg, title){
+  axios({
+    method: 'get',
+    url: 'https://api.hackertarget.com/pagelinks/?q='+link,
+    responseType: 'JSON'
+  })
+    .then(function (response) {
+      console.log(response.data);
+      let results = response.data.match(albumRegex);
+      // go through the links in results and return the right link
+      var i;
+      for(i = 0; i<results.length; i++){
+        getHTMLfor(results[i], msg, title)
+      }
+      setTimeout(function(){
+        if(i == results.length && found == false){
+          // song wasn't found, blame my debt.
+          bot.sendMessage(msg.chat.id, "Oopsie. We hit a snag trying to get your song. Blame the lack of an Apple Music API key. If you want to contribute to @amcdevitt97 's college kid API fund, venmo me 99 dollars at venmo.com/pay-here", {});
+        }
+      }, 5000);
+      
+    })
+    .catch(function (error) {
+      // Error hit
+      bot.sendMessage(msg.chat.id, "Oopsie. We hit a snag trying to get your song. If you see @amcdevitt97, tell her this error happened: "+ error, {});
+    });;
+}
+
+// look for the link that has a title that matches our song
+function getHTMLfor(link, msg, title){
+  axios.get(link).then(response => {
+    console.log(title);
+    // Site has a title tag with our song title in it
+    if(response.data.toString().toLowerCase().includes('<title>‎'+title.toLowerCase())){
+      found = true;
+      console.log('FOUND');
+      bot.sendMessage(msg.chat.id, "Apple Music Link: "+ link, {});
+    }
+    else{
+      console.log('NOT THIS');
+    }
+  })
+  .catch(error => {
+    console.log(error);
+  })
+}
+
 
 // ----------------- END APPLE MUSIC SEARCH
 
@@ -70,44 +141,10 @@ bot.on('text', function (msg){
     // Lookup title and artist
     spotifyApi.getTrack(spotifyID)
     .then(function (data) {
-        var query = data.body.name + ' ';
-        
+        var title = data.body.name;
+        var query = title + ' ';
         data.body.artists.forEach(artist => { query+= artist.name; });
-
-        runSearch(query).then(function(link){
-          var artistRegex = /https?:\/\/(music\.)?apple\.com\/([-a-zA-Z]{2})\/artist\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
-          var albumRegex = /https?:\/\/(music\.)?apple\.com\/([-a-zA-Z]{2})\/album\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
-          if(link.match(artistRegex)){
-            axios({
-              method: 'get',
-              url: 'https://api.hackertarget.com/pagelinks/?q='+link,
-              responseType: 'JSON'
-            })
-              .then(function (response) {
-                console.log(response.data);
-                let results = response.data.match(albumRegex);
-                // TODO: go through the links in results and look for one that has a title that matches our song
-                for(var i = 0; i<results.length; i++){
-                  bot.sendMessage(msg.chat.id, results[i], {});
-                }
-              })
-              .catch(function (error) {
-                bot.sendMessage(msg.chat.id, "Oopsie. We hit a snag trying to get your song. If you see @amcdevitt97, tell her this error happened: "+ error, {});
-              });;
-            
-          }
-          else if(link.match(albumRegex)){
-            // TODO : store all list items (li) with ' "targetId": ' in their 
-            // properties list, and put all the ones with numbers following 
-            // it into an array. Pull the spotify track number and get the
-            // song id from the given array based on that track number. 
-            bot.sendMessage(msg.chat.id, "ALBUM LINK: "+ link, {});
-          }
-          
-          // This is excessive scraping but this is what Apple gets
-          // when they make their Music API 99 dollars to use... 
-        });
-
+        searchAppleMusicLink(query, msg, title);
     }, function (err) {
         console.log('Something went wrong!', err);
     });
