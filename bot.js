31660aad715c5f6b3875191f2a4a19e7a787f28d
amcdevitt97 <amcdevitt97@gmail.com>
2019-09-22 18:10:35 -0400

    working things around

--- bot.js
+++ bot.js
@@ -1,10 +1,9 @@
+// TODO: generate new bot token
 var TelegramBot = require('node-telegram-bot-api'); 
-//var token = require('./config/config.js').authToken;
-const bot = new TelegramBot(process.env.token, {polling: true});
+var token = process.env.token;
+const bot = new TelegramBot(token, {polling: true});
 const appleMusic = require('./applemusic');
 const spotify = require('./spotify');
-// TODO: generate new bot token
-
 
 
 
